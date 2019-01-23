67ee69d30b7d79fff61eeac74b2699adee83f389
dvlwj <dvlimweijiang@gmail.com>
2019-01-23 22:56:56 +0700

    Prepare login test case

---
+++ tests/API/LoginAPITest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\API;
+
+use Tests\HttpClient;
+use PHPUnit\Framework\TestCase;
+
+class LoginAPITest extends TestCase
+{
+    use HttpClient;
+
+    /**
+     *  @return void
+     */
+    public function testGetCsrfToken(): void
+    {
+
+        $this->assertTrue();
+    }
+}
---
+++ tests/HttpClient.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests;
+
+trait HttpClient
+{
+    public function _exec(string $url, array $opt = []): array
+    {
+        $ch = curl_init($url);
+        $optf = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_USERAGENT => "HttpClient"
+        ];
+        foreach ($opt as $k => &$v) {
+            $optf[$k] = $v;
+        }
+        unset($opt, $k, $v);
+        curl_setopt_array($ch, $opt);
+        $out = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        $err = curl_error($ch);
+        $ern = curl_errno($ch);
+        curl_close($ch);
+        $ch = null;
+        unset($ch);
+        return [
+            "out" => $out,
+            "info" => $info,
+            "error" => $err,
+            "ern" => $ern
+        ];
+    }
+}
